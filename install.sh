21730aa2f8393de7bb058f0b3df2078a5a25f81f
SKGHD <sangramgorai@gmail.com>
2020-09-06 02:12:52 +0530

    fixed few things

--- install.sh
+++ install.sh
@@ -1,18 +1,16 @@
 #!/data/data/com.termux/files/usr/bin/bash
 pkg install wget tar gzip -y
 
-mkdir heroku
 wget http://cli-assets.heroku.com/heroku-linux-arm.tar.gz -O heroku.tar.gz
-tar -xvzf heroku.tar.gz -C heroku
+tar -xvzf heroku.tar.gz
 mkdir -p /data/data/com.termux/files/usr/lib/heroku
 mv heroku/* /data/data/com.termux/files/usr/lib/heroku
 ln -s /data/data/com.termux/files/usr/lib/heroku/bin/heroku /data/data/com.termux/files/usr/bin/heroku
 cd /data/data/com.termux/files/usr/lib/heroku/bin/
 sed -i 's/#!/#!\/data\/data\/com.termux\/files/g' heroku
 pkg install nodejs -y
-cd /data/data/com.termux/files/usr/lib/heroku/bin
 mv node node.old
 ln -s ../../../bin/node node
 echo 'export PATH="/data/data/com.termux/files/usr/lib/heroku/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
-heroku login -i
+heroku --version
